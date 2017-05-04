de9bf02bbd5c519002dd2dae94ef834fc4a85d6e
Trung Do <trung.dothanh@toshiba-tsdv.com>
2017-05-04 17:11:57 +0700

    Add config files for 'reprepro' and script to run sbuild
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

---
+++ sbuild-loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+trap "exit" INT
+rm -f sbuild-status
+for pkg in `cat pkglist | cut -d' ' -f1`; do
+	dir=`find repo/pool/ -name $pkg`
+	if [ "$dir" = "" ]; then continue; fi
+	cd $dir
+
+	ver=`cat $pkg_*.dsc | grep "^Version:"|head -1|cut -d' ' -f2 | sed -e "s/.*://"`
+
+	rm -rf *.build *.buildinfo *.deb *.changes
+	sbuild -j 16 --host=armhf -d unstable-amd64-sbuild $pkg_*.dsc
+	buildlog=${pkg}_${ver}_armhf.build
+	output="$pkg $ver $(grep "^Status:" $buildlog | cut -d' ' -f2) $(grep "Finished at" $buildlog|tail -1|sed -e "s/Finished at //")"
+	echo $output >> sbuild-status
+
+	cd -
+done
