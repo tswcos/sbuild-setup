175a59dba8668021a94135174b260624a01e496e
Trung Do <trung.dothanh@toshiba-tsdv.com>
2017-05-05 14:38:40 +0700

    sbuild-loop.sh: Clean *.udeb files before build
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

--- sbuild-loop.sh
+++ sbuild-loop.sh
@@ -9,7 +9,7 @@ for pkg in `cat pkglist | cut -d' ' -f1`; do
 
 	ver=`cat $pkg_*.dsc | grep "^Version:"|head -1|cut -d' ' -f2 | sed -e "s/.*://"`
 
-	rm -rf *.build *.buildinfo *.deb *.changes
+	rm -rf *.build *.buildinfo *.udeb *.deb *.changes
 	sbuild -j 16 --host=armhf -d unstable-amd64-sbuild $pkg_*.dsc
 	buildlog=${pkg}_${ver}_armhf.build
 	output="$pkg $ver $(grep "^Status:" $buildlog | cut -d' ' -f2) $(grep "Finished at" $buildlog|tail -1|sed -e "s/Finished at //")"
