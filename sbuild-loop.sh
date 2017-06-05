03520ff4ff41afe52d7bec08a18c425dfe1eaec9
Trung Do <trung.dothanh@toshiba-tsdv.com>
2017-06-05 14:08:31 +0700

    sbuild: Add option --profiles=cross
    Some packages (like guile-2.0) require profile "cross" to install dependencies.
    
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

--- sbuild-loop.sh
+++ sbuild-loop.sh
@@ -16,7 +16,7 @@ for pkg in $last_update_pkgs; do
 	ver=`cat $pkg_*.dsc | grep "^Version:"|head -1|cut -d' ' -f2 | sed -e "s/.*://"`
 
 	rm -rf *.build *.buildinfo *.udeb *.deb *.changes
-	sbuild -j 16 --host=armhf -d unstable-amd64-sbuild $pkg_*.dsc
+	sbuild --jobs=16 --profiles=cross --host=armhf -d unstable-amd64-sbuild $pkg_*.dsc
 
 	buildlog=${pkg}_${ver}_armhf.build
 	output="$pkg $ver $(grep "^Status:" $buildlog | cut -d' ' -f2) $(grep "Finished at" $buildlog|tail -1|sed -e "s/Finished at //")"
