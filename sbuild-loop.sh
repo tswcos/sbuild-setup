9841b9c1dfa06fdea807f05603e7d3da22c2d757
Trung Do <trung.dothanh@toshiba-tsdv.com>
2017-06-09 10:49:57 +0700

    Specific build profile for package
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

--- sbuild-loop.sh
+++ sbuild-loop.sh
@@ -2,11 +2,13 @@
 
 trap "exit" INT
 
+build_profile="${PWD}/build-profile"
 sbuild_status="${PWD}/sbuild-status"
 repo_logfile="${PWD}/repo/logs/logfile"
 repo_last_update_date=$(awk '{print $1}' $repo_logfile | tail -1)
 last_update_pkgs=$(grep $repo_last_update_date $repo_logfile | awk '{print $8}')
 
+touch $build_profile
 touch $sbuild_status
 for pkg in $last_update_pkgs; do
 	dir=`find repo/pool/ -name $pkg`
@@ -14,9 +16,14 @@ for pkg in $last_update_pkgs; do
 	cd $dir
 
 	ver=`cat $pkg_*.dsc | grep "^Version:"|head -1|cut -d' ' -f2 | sed -e "s/.*://"`
+	profile=`grep "^$pkg " $build_profile | cut -d' ' -f2`
+	profile_option=""
+	if [ "$profile" != "" ]; then
+		profile_option="--profiles=$profile"
+	fi
 
 	rm -rf *.build *.buildinfo *.udeb *.deb *.changes
-	sbuild --jobs=16 --profiles=cross --host=armhf -d unstable-amd64-sbuild $pkg_*.dsc
+	sbuild --jobs=16 ${profile_option} --host=armhf -d unstable-amd64-sbuild $pkg_*.dsc
 
 	buildlog=${pkg}_${ver}_armhf.build
 	output="$pkg $ver $(grep "^Status:" $buildlog | cut -d' ' -f2) $(grep "Finished at" $buildlog|tail -1|sed -e "s/Finished at //")"
